597de4238f2b2e7cbdd4fff493d6a6ea72d88440
prioarief <prioariefgunawangunawan@gmail.com>
2020-08-08 19:15:32 +0700

    prepare login

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -5,7 +5,14 @@ const {
 	LoginValidation,
 	VerifyValidation,
 } = require('../helpers/validation');
-const { Register, Login, insertOTP, checkOTP, deleteOTP, updateUser } = require('../models/Auth');
+const {
+	Register,
+	Login,
+	insertOTP,
+	checkOTP,
+	deleteOTP,
+	updateUser,
+} = require('../models/Auth');
 const randomCode = require('randomatic');
 const { sendOTP } = require('../helpers/sendEmail');
 
@@ -33,6 +40,7 @@ module.exports = {
 			}
 			let errorMessage = validation.error.details[0].message;
 			errorMessage = errorMessage.replace(/"/g, '');
+			return response(res, false, errorMessage, 401);
 		} catch (error) {
 			console.log(error);
 			return response(res, false, 'Internal Server Error', 500);
@@ -56,8 +64,8 @@ module.exports = {
 							const update = {
 								is_active: 1,
 							};
-							await updateUser(update, data.email)
-							await deleteOTP(data.email)
+							await updateUser(update, data.email);
+							await deleteOTP(data.email);
 							return response(res, true, 'Verification Success', 200);
 						}
 						return console.log('no');
@@ -68,6 +76,22 @@ module.exports = {
 			let errorMessage = validation.error.details[0].message;
 			errorMessage = errorMessage.replace(/"/g, '');
 			console.log(errorMessage);
+		} catch (error) {
+			console.log(error);
+		}
+	},
+	Login: async (req, res) => {
+		try {
+			const data = req.body;
+			const validation = LoginValidation(data);
+			if (validation.error === undefined) {
+				const emailCheck = await Login(data.email);
+
+				if (emailCheck.length !== 0) {
+					return console.log(emailCheck);
+				}
+			}
+			// const emailCheck = await Login
 		} catch (error) {
 			console.log(error);
 		}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { Register, Verification } = require('../controllers/AuthController');
+const {
+	Register,
+	Verification,
+	Login,
+} = require('../controllers/AuthController');
 const router = express.Router();
 
 router.post('/register', Register);
 router.post('/verify', Verification);
+router.post('/login', Login);
 
 module.exports = router;
