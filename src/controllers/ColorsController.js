546bee77da82761f0f91b83f239e54542bd2cd67
Dimas Mokodompit <dimasdompit@gmail.com>
2020-08-10 22:43:03 +0700

    fix validation edit colors

--- src/controllers/ColorsController.js
+++ src/controllers/ColorsController.js
@@ -72,15 +72,23 @@ module.exports = {
   updateColors: async (req, res) => {
     const data = req.body;
     const id = req.params.id;
+    const oldData = await getColorByNameModel(data.color);
+    const existName = {
+      ...oldData[0],
+    };
     try {
-      const validation = UpdateColorsValidation(data);
-      if (validation.error === undefined) {
-        const result = await updateColorsModel(data, id);
-        return response(res, true, result, 200);
+      if (data.color === existName.color) {
+        return response(res, false, `Color name is already exist!`, 401);
+      } else {
+        const validation = UpdateColorsValidation(data);
+        if (validation.error === undefined) {
+          const result = await updateColorsModel(data, id);
+          return response(res, true, result, 200);
+        }
+        let errorMsg = validation.error.details[0].message;
+        errorMsg = errorMsg.replace(/"/g, "");
+        return response(res, false, errorMsg, 400);
       }
-      let errorMsg = validation.error.details[0].message;
-      errorMsg = errorMsg.replace(/"/g, "");
-      return response(res, false, errorMsg, 400);
     } catch (error) {
       console.log(error);
       return response(res, false, "Internal Server Error", 500);
